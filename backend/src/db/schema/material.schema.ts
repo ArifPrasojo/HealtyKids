64c7b07e187987ed8c8a0955f48c4a7f90fcde83
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-12-10 13:23:10 +0700

    Fix: Perbaikan schema database

--- backend/src/db/schema/material.schema.ts
+++ backend/src/db/schema/material.schema.ts
@@ -1,12 +1,11 @@
 import { pgTable, serial, text, varchar, integer, boolean, timestamp } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
-import { users } from './user.schema'
 import { subMaterial } from './sub-material.schema'
 
 export const materials = pgTable("materials", {
     id: serial("id").primaryKey(),
-    title: varchar("title").notNull(),
-    description: text("description"),
+    title: varchar("title", { length: 100 }).notNull(),
+    description: varchar("description", { length: 150 }),
     isDelete: boolean("is_delete").notNull().default(false),
     createdAt: timestamp("created_at").notNull().defaultNow(),
     updatedAt: timestamp("updated_at")
