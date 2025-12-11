ec13df230c3f020731430059e5a71988cd3fb42a
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-12-11 13:26:04 +0700

    Fix: Perbaikan validator

--- backend/src/modules/materials/material.validator.ts
+++ backend/src/modules/materials/material.validator.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod'
 
 export const createMaterialSchema = z.object({
-    title: z.string().min(1, "Nama harus diisi").max(255, "Maksimal 255 karakter"),
-    desctiption: z.string().min(1, "Nama harus diisi").max(255, "Maksimal 255 karakter")
+    title: z.string().nonempty("Title Harus Diisi").max(255, "Maksimal 255 karakter"),
+    description: z.string().nonempty("Deskripsi Harus Diisi").max(255, "Maksimal 255 karakter")
 })
 
 export const updateMaterialSchema = z.object({
-    title: z.string().min(1, "Nama harus diisi").max(255, "Maksimal 255 karakter"),
-    desctiption: z.string().min(1, "Nama harus diisi").max(255, "Maksimal 255 karakter")
+    title: z.string().nonempty("Title Harus Diisi").max(255, "Maksimal 255 karakter"),
+    description: z.string().nonempty("Deskripsi Harus Diisi").max(255, "Maksimal 255 karakter")
 })
