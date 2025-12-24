751db136eac71607b2969073e6c0c07642033706
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-12-24 16:04:54 +0700

    Feat: Buat fitur get quiz student

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from "pg";
+import * as schema from '@/db/schema'
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
-const db = drizzle(pool)
+const db = drizzle(pool, { schema })
 
 export { db };
